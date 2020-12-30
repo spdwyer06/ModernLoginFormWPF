92e8d1d54a0503127e9bf12dd546aa21ec218f2a
Sean Dwyer <spdwyer06@gmail.com>
2020-12-30 13:46:00 -0500

    Initial commit

---
+++ LoginFormDemo_UI/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginFormDemo_UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //ImageBrush formLogo = new ImageBrush();
+            //formLogo.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Assets/GeometricHeader2.png"));
+            //logo.Fill = formLogo;
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+    }
+}
